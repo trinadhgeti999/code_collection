e6f3c8ac1736befd3b3950a20558acc0d7c7997e
trinadhgeti999 <119048891+trinadhgeti999@users.noreply.github.com>
2025-10-18 14:43:48 +0530

    removeDuplicates

---
+++ removeDuplicates.java
@@ -0,0 +1,23 @@
+package Practice;
+
+//for sorted array
+public class removeDuplicates {
+    public static int remove(int[] arr){
+        int i=0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[j]!=arr[i]){
+                arr[i+1]=arr[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,2,7,9,9,10};
+        int filterArray=remove(arr);
+        for(int i=0;i<filterArray;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
