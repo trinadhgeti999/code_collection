0684bc3fe70c4679f16b0ae605487900c4b12f1f
trinadhgeti999 <119048891+trinadhgeti999@users.noreply.github.com>
2025-10-21 20:12:12 +0530

    Added optimal solution

--- longestSubArrayWithSum_K.java
+++ longestSubArrayWithSum_K.java
@@ -1,6 +1,45 @@
 package Practice;
 
+import java.util.Map;
+import java.util.HashMap;
+
+public class longestSubArrayWithSum_K {
+    public static int findLongestSubArrayLength(int[] arr,int k){
+        Map<Integer,Integer> map=new HashMap<>();
+
+        int max_length=0;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            
+            if(sum==k){
+                max_length=i+1;
+            }
+
+            if(map.containsKey(sum-k)){
+                int length=i-map.get(sum-k);
+                if(length>max_length){
+                    max_length=length;
+                }
+            }
+
+            if(!map.containsKey(sum)){
+                map.put(sum, i);
+            }
+        }
+        return max_length;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,1,1,1,1,1,0,4,2,3};
+        int k=0;
+        int length=findLongestSubArrayLength(arr,k);
+        System.out.println(length);
+    }
+}
+
 //brute
+/*
 public class longestSubArrayWithSum_K {
     public static int findLongestSubArrayLength(int[] arr,int k){
         int longest_length=0;
@@ -32,3 +71,5 @@ public class longestSubArrayWithSum_K {
         System.out.println(length);
     }
 }
+
+*/
