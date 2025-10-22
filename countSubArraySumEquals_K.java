660aec35734dd40d9ce508cf5083b74450403df7
trinadhgeti999 <119048891+trinadhgeti999@users.noreply.github.com>
2025-10-22 16:55:05 +0530

    Added optimal solution

--- countSubArraySumEquals_K.java
+++ countSubArraySumEquals_K.java
@@ -1,8 +1,49 @@
 package Practice;
 
+import java.util.Map;
+import java.util.HashMap;
+
+public class countSubArraySumEquals_K {
+    public static int findcount(int[] arr,int k){
+       Map<Integer,Integer> map=new HashMap<>();
+
+       int sum=0;
+       int count=0;
+       map.put(0, 1);//instead we can use if condition below
+
+       for(int i=0;i<arr.length;i++){
+        sum+=arr[i];
+
+        // if(sum==k){//
+        //     count++;
+        // }
+
+        if(map.containsKey(sum-k)){
+            count+=map.get(sum-k);
+        }
+
+        map.put(sum,map.getOrDefault(sum,0)+1);
+
+       }
+       return count;
+    }
+
+    
+    public static void main(String[] args) {
+        int arr[]={1,-1,0};
+        int k=0;
+        int countSubArraySums=findcount(arr,k);
+        System.out.println(countSubArraySums);
+    }
+}
+
+
+
+
+/*
 //brute
 public class countSubArraySumEquals_K {
-    public static int findLongestSubArrayLength(int[] arr,int k){
+    public static int findcount(int[] arr,int k){
 
         int count=0;
         int sum=0;
@@ -21,7 +62,8 @@ public class countSubArraySumEquals_K {
     public static void main(String[] args) {
         int arr[]={1,2,3,-3,1,1,1,4,2,-3};
         int k=3;
-        int length=findLongestSubArrayLength(arr,k);
+        int countSubArraySums=findcount(arr,k);
         System.out.println(length);
     }
 }
+     */
