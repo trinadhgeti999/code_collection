f853c936d7a52b5c10c04d61506a3f1eb2fadcd3
trinadhgeti999 <119048891+trinadhgeti999@users.noreply.github.com>
2025-10-22 22:39:31 +0530

    Added optimal solution

--- missingNumber.java
+++ missingNumber.java
@@ -3,6 +3,25 @@ package Practice;
 //brute-linear search
 //and
 
+//optimal solution
+public class missingNumber {
+    public static int findMissingNumber(int[] arr){
+        
+        int totalSum=(arr.length*(arr.length+1))/2;
+        int arrSum=0;
+        for(int i=0;i<arr.length;i++){
+            arrSum+=arr[i];
+        }
+        return totalSum-arrSum;
+    }
+    public static void main(String[] args) {
+        int arr[]={9,6,4,2,3,5,7,0,1};
+        int missingNumber=findMissingNumber(arr);
+        System.out.println(missingNumber);
+    }
+}
+
+/*
 //better-hashing
 public class missingNumber {
     public static int findMissingNumber(int[] arr){
@@ -25,3 +44,7 @@ public class missingNumber {
         System.out.println(missingNumber);
     }
 }
+
+*/
+
+
