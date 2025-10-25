1a46c8b8c258dafb0dd6700e77151ebe5c14a753
trinadhgeti999 <119048891+trinadhgeti999@users.noreply.github.com>
2025-10-25 13:27:56 +0530

    Rotate Matrix By 90 degrees

--- rotateMatrixBy90.java
+++ rotateMatrixBy90.java
@@ -1,5 +1,39 @@
 package Practice;
 
+//inplace - for square matrix only
+
+public class rotateMatrixBy90{
+    public static void main(String[] args) {
+        int matrix[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int n=matrix.length;
+        int m=matrix.length/2;
+
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-(j+1)];
+                matrix[i][n-(j+1)]=temp;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+
 public class rotateMatrixBy90 {
     public static void main(String[] args) {
         int[][] matrix={{1,1,1,1},{1,0,0,1},{1,1,0,1},{1,1,1,1}};
@@ -8,9 +42,10 @@ public class rotateMatrixBy90 {
 
         for(int i=0;i<m;i++){
             for(int j=n-1;j>=0;j--){
-                System.out.print(matrix[j][i]+" ");//or we can store in other 2d array
+                System.out.print(matrix[j][i]+" ");//need other 2d array to store
             }
             System.out.println();
         }
     }
 }
+*/
