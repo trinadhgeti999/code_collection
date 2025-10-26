0ffc9364474e4fb05a6c2cad8f97b5b0a6544d1c
trinadhgeti999 <119048891+trinadhgeti999@users.noreply.github.com>
2025-10-26 12:42:48 +0530

    Added optimal solution

--- maxNo_OfVowelsInSubstring.java
+++ maxNo_OfVowelsInSubstring.java
@@ -2,6 +2,62 @@ package Practice;
 
 //Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
 
+public class maxNo_OfVowelsInSubstring {
+    public static int maxVowels(String s,int k) {
+
+        int maxCount=0;
+        for(int i=0;i<k;i++){
+            char ch=s.charAt(i);
+            if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                maxCount++;
+            }
+        }
+
+        int currentcount=maxCount;
+        int i=0;
+        int j=k;
+
+        while (j<s.length()) {
+
+            if(maxCount==k) return maxCount;
+
+            if(checkvowel(s.charAt(i))){
+                currentcount--;
+            }
+
+            if(checkvowel(s.charAt(j))){
+                currentcount++;
+            }
+
+            if(currentcount>maxCount){
+                maxCount=currentcount;
+            }
+
+            i++;
+            j++;
+
+        }
+        return maxCount;
+    }
+
+    public static boolean checkvowel(char ch){
+        if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+            return true;
+            }
+            return false;
+    }
+    
+    public static void main(String[] args) {
+        String s = "zpuerktejfp";
+        int k = 3;
+
+        int result=maxVowels(s,k);
+        System.out.println(result);
+
+    }
+}
+
+/*
 //brute
 public class maxNo_OfVowelsInSubstring {
     public static int maxVowels(String s,int k) {
@@ -38,3 +94,5 @@ public class maxNo_OfVowelsInSubstring {
 
     }
 }
+
+*/
